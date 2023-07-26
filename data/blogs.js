40837a0c4bb158698b65871d03d9ab930f4b5b4a
Mahbubur Rahman <77193562+mahbubcsebd@users.noreply.github.com>
2023-07-26 17:30:55 +0600

    update

---
+++ data/blogs.js
@@ -0,0 +1,48 @@
+import team1 from '../public/images/team-1.png';
+
+const teams = [
+  {
+    id: 1,
+    title: "Logistics vs. Supply Chain: What's the Difference?",
+    img: team1,
+    tag: "courier"
+    desc: 'Marketing Coordinator',
+  },
+  {
+    id: 1,
+    title: "Logistics vs. Supply Chain: What's the Difference?",
+    img: team1,
+    tag: "courier"
+    desc: 'Marketing Coordinator',
+  },
+  {
+    id: 1,
+    title: "Logistics vs. Supply Chain: What's the Difference?",
+    img: team1,
+    tag: "courier"
+    desc: 'Marketing Coordinator',
+  },
+  {
+    id: 1,
+    title: "Logistics vs. Supply Chain: What's the Difference?",
+    img: team1,
+    tag: "courier"
+    desc: 'Marketing Coordinator',
+  },
+  {
+    id: 1,
+    title: "Logistics vs. Supply Chain: What's the Difference?",
+    img: team1,
+    tag: "courier"
+    desc: 'Marketing Coordinator',
+  },
+  {
+    id: 1,
+    title: "Logistics vs. Supply Chain: What's the Difference?",
+    img: team1,
+    tag: "courier"
+    desc: 'Marketing Coordinator',
+  },
+];
+
+export default teams;
